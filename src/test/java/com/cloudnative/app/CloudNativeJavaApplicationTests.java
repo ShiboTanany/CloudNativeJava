536983df99e9f7ae3db4e77f6d0e75b13c59a44f
ShaabanEltanany <shaaban.eltanany.ext@orange.com>
2020-09-15 21:51:09 +0200

    initial spring boot application

---
+++ src/test/java/com/cloudnative/app/CloudNativeJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cloudnative.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudNativeJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
